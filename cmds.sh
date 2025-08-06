c3b8c84ce07d701f6ada501e7ad935c7379dcbf0
miki <mikirub8@gmail.com>
2025-08-06 11:03:57 +0300

    initial commit

---
+++ cmds.sh
@@ -0,0 +1,6 @@
+helm repo add jenkins https://charts.jenkins.io
+helm repo update
+helm install jenkins
+kubectl exec --namespace default -it svc/jenkins -c jenkins -- /bin/cat /run/secrets/
+additional/chart-admin-password && echo
+kubectl port-forward svc/jenkins 8080:8080 &
