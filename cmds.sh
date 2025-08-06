988680b32f083d971f83efa5d37808955d59fc4b
miki <mikirub8@gmail.com>
2025-08-06 11:21:50 +0300

    adding charts

--- cmds.sh
+++ cmds.sh
@@ -4,3 +4,8 @@ helm install jenkins
 kubectl exec --namespace default -it svc/jenkins -c jenkins -- /bin/cat /run/secrets/
 additional/chart-admin-password && echo
 kubectl port-forward svc/jenkins 8080:8080 &
+
+
+helm install rabbitmq bitnami/rabbitmq -f rabbitmq-values.yaml
+helm install consumer consumer-chart/
+helm install producer producer-chart/
